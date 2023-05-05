8953b70ddd2eddeec3b7cfaa8c4b1c7a3c8894a1
StIgorek <elitelifegroup@gmail.com>
2023-05-05 12:13:36 +0300

    task-01 update

--- js/task-01.js
+++ js/task-01.js
@@ -1,20 +1,27 @@
 
-const categoriesRef = document.getElementById('categories');
+//const categoriesRef = document.getElementById('categories');
 
-console.log('Number of categories: ', categoriesRef.children.length);
+//console.log('Number of categories: ', categoriesRef.children.length);
+
+//const categoryList = categoriesRef.querySelectorAll('.item');
+//categoryList.forEach( item => {
+  //  const categoryName = item.querySelector('h2').textContent;
+   // const elementsCount = item.querySelectorAll('li').length;
+   // console.log(`Category: ${categoryName}`);
+   // console.log(`Elements: ${elementsCount}`);
+//});
+//не використані властивості навігації по ДОМ.
+
+const categoriesRef = document.getElementById('categories');
+console.log('Number of categories: ', categoriesRef.childElementCount);
 
 const categoryList = categoriesRef.querySelectorAll('.item');
 categoryList.forEach( item => {
-    const categoryName = item.querySelector('h2').textContent;
-    const elementsCount = item.querySelectorAll('li').length;
-    console.log(`Category: ${categoryName}`);
-    console.log(`Elements: ${elementsCount}`);
+    const categoryName = item.firstElementChild.textContent;
+    const elementsCount = item.lastElementChild.childElementCount;
+console.log(`Category: ${categoryName}`);
+console.log(`Elements: ${elementsCount}`);
 });
-
-
-
-
-
 
 
 
