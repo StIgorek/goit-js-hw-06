66512238b77d11e22e84ee25ece02c3bdea0b0f1
StIgorek <elitelifegroup@gmail.com>
2023-05-11 19:24:04 +0300

    update task-07

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,14 @@
 
 const fontSizeControlRef = document.getElementById('font-size-control');
-const textSizeRef = document.getElementById('text');
+let textSizeRef = document.getElementById('text');
+
 
 fontSizeControlRef.addEventListener('input', onSizeChange);
 
 function onSizeChange(event) {
   textSizeRef.style.fontSize = `${event.currentTarget.value}px`
 };
+
+//Завдання 7 - потрібно в початковий фонтсайз спана поставити 
+//початкове значення інпута, щоб не було скачків розміру текста 
+//при пересуванні повзунка.
